061e1222e887ecf562a5b8e609e597627e03e57d
numberbook <51064616+numberbook@users.noreply.github.com>
2019-05-28 11:50:32 +0800

    4567

--- src/com/qfedu/App.java
+++ src/com/qfedu/App.java
@@ -3,6 +3,6 @@ package com.qfedu;
 public class App {
     public static void main(String[] args) {
         System.out.println("s");
-        System.out.println("sdfghj");
+        System.out.println("s");
     }
 }
